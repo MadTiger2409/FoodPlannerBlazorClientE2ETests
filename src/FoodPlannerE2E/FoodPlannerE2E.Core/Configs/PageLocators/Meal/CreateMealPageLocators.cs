c204c3d388c2fd568dcd40b3c6441bab09e476e4
Michał B <MadTiger2409@users.noreply.github.com>
2022-10-13 15:14:45 +0200

    Another page locators

---
+++ src/FoodPlannerE2E/FoodPlannerE2E.Core/Configs/PageLocators/Meal/CreateMealPageLocators.cs
@@ -0,0 +1,17 @@
+﻿namespace FoodPlannerE2E.Core.Configs.PageLocators.Meal
+{
+    public class CreateMealPageLocators
+    {
+        public string NameTextInput { get; set; }
+        public string FormErrorMessages { get; set; }
+        public string IngredientProductTypeahead { get; set; }
+        public string IngredientProductErrorMessage { get; set; }
+        public string TypeaheadResults { get; set; }
+        public string IngredientAmountNumberInput { get; set; }
+        public string IngredientAmountErrorMessage { get; set; }
+        public string IngredientUnitTypeahead { get; set; }
+        public string IngredientUnitErrorMessage { get; set; }
+        public string AddIngredientButton { get; set; }
+        public string SendButton { get; set; }
+    }
+}
---
+++ src/FoodPlannerE2E/FoodPlannerE2E.Core/Configs/PageLocators/PlannedMeal/PlannedMealsListPageLocators.cs
@@ -0,0 +1,10 @@
+﻿namespace FoodPlannerE2E.Core.Configs.PageLocators.PlannedMeal
+{
+    public class PlannedMealsListPageLocators
+    {
+        public string StartDateInput { get; set; }
+        public string EndDateInput { get; set; }
+        public string SendButton { get; set; }
+        public string FormErrorMessages { get; set; }
+    }
+}
